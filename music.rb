76519bffe00b6196125aec3d2aa50f7e8a6764af
Robert Christensen <rchristensen@wesleyan.edu>
2014-07-03 01:16:05 -0400

    It's working!
    Pretty much a YAML expert now.. (not)

--- music.rb
+++ music.rb
@@ -4,12 +4,22 @@ require 'yaml'
 # Read our file, gives us a hash that fulfills first challenge requirement.
 data = YAML.load_file 'music.yml'
 
-# Modify hash to satisfy second challenge requirement.
-def data.method_missing(n)
-  self[n.to_s]
+# Modify Hash class to satisfy second challenge requirement.
+class Hash
+  def method_missing(meth)
+    self[meth.to_s]
+  end
 end
 
-# Just some debugging.
+# Just some debugging/tests..
+#
+# Output hash of YAML
 puts data.inspect
+# Output genres
 puts data['genres']
+# Output genres via method
 puts data.genres
+# Output challenge requirement #1
+puts data['genres'].last['artists'].first['albums'].first['tracks'].last['name']
+# Output challenge requirement #2
+puts data.genres.last.artists.first.albums.first.tracks.last.name
