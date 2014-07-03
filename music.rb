bd4444a474ebc6690200cd77d9230eff71137d0a
Robert Christensen <rchristensen@wesleyan.edu>
2014-07-03 01:19:01 -0400

    Punctuation

--- music.rb
+++ music.rb
@@ -6,7 +6,7 @@ require 'yaml'
 data = YAML.load_file 'music.yml'
 
 # Modify Hash class to satisfy second challenge requirement.
-# Essentially, if no method is defined passes argument to any hash as a value
+# Essentially, if no method is defined passes argument to any hash as a value.
 # So data.genres will yield the same output as data['genres'].
 class Hash
   def method_missing(meth)
@@ -14,15 +14,15 @@ class Hash
   end
 end
 
-# Just some debugging/tests..
+# Just some debugging/tests.
 #
-# Output hash of YAML
+# Output hash of YAML.
 puts data.inspect
-# Output genres
+# Output genres.
 puts data['genres']
-# Output genres via method
+# Output genres via method.
 puts data.genres
-# Output challenge requirement #1
+# Output challenge requirement #1.
 puts data['genres'].last['artists'].first['albums'].first['tracks'].last['name']
-# Output challenge requirement #2
+# Output challenge requirement #2.
 puts data.genres.last.artists.first.albums.first.tracks.last.name
