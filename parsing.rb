f0015bb9beca328f1a7cfd03c1f9c1dd45174839
ellen333712 <tian.ellen.yang@gmail.com>
2014-09-18 16:19:43 -0400

    Update parsing.rb

--- parsing.rb
+++ parsing.rb
@@ -2,6 +2,9 @@ require 'yaml'
 require 'ostruct'
 
 hashed_data = YAML.load_file('music.yml')
+# had a misunderstanding of the load_file function, thought it could return a hash of the current yaml file, and continued on that thinking to work.
+# And found that my class does work for hash, but not for the return value of load_file. I will think about this issue, and hope to have a discussion
+# with you.
 
 class IterativeStruct < OpenStruct
     def initialize(hash=nil)
@@ -13,9 +16,11 @@ class IterativeStruct < OpenStruct
     end
 
     def first
+    	
     end
 
     def last
+    	
     end
 end
 
