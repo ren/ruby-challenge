59e97c7c5bf44d7b2cdbde722b624fae14b34477
ellen333712 <tian.ellen.yang@gmail.com>
2014-09-18 15:05:20 -0400

    Update parsing.rb

--- parsing.rb
+++ parsing.rb
@@ -3,7 +3,7 @@ require 'ostruct'
 
 data = OpenStruct.new(YAML.load_file('music.yml'))
 
-
+#data will be returned as hash, so []operation works, but . operation does not
 puts data
 puts data["genres"].last["artists"].first["albums"].first["tracks"].last["name"]
 puts data.genres.last
